2780c14b08dab0b25090de8ca70866bdf6f19ee1
Ke Chen <kechen1.ck@gmail.com>
2019-04-24 11:12:20 -0400

    process segmented data with behavioral data

---
+++ batch_CNMF_v2.m
@@ -0,0 +1,42 @@
+
+clear
+close all
+file = 'RVKC368';
+date = {'180731', '180802','180807','180809','180814'};
+for i = 1:length(date)
+    imaging_analysis_GC_v6(file,date{i})
+end
+%%
+close all
+file = 'RVKC377';
+date = {'180821', '180823','180828'};
+for i = 1:length(date)
+    imaging_analysis_GC_v6(file,date{i})
+end
+%%
+file = 'RVKC402';
+date = {'181022', '181024','181029'};
+for i = 1:length(date)
+    imaging_analysis_GC_v6(file,date{i})
+end
+%%
+close all
+file = 'RVKC403';
+date = {'181022', '181024','181029','181030','181031'};
+for i = 1:length(date)
+    imaging_analysis_GC_v6(file,date{i})
+end
+%%
+close all
+file = 'RVKC404';
+date = {'181112', '181114','181119'};
+for i = 1:length(date)
+    imaging_analysis_GC_v6(file,date{i})
+end
+%%
+close all
+file = 'RVKC405';
+date = {'181112', '181114','181119','181120'};
+for i = 1:length(date)
+    imaging_analysis_GC_v6(file,date{i})
+end
