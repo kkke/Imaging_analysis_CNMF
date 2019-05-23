f418549484b8d860426b5d43c1b9d1585175e7bc
Ke Chen <kechen1.ck@gmail.com>
2019-05-23 14:00:56 -0400

    add description

--- Manual_ROI_corrrection.m
+++ Manual_ROI_corrrection.m
@@ -3,7 +3,7 @@ load('data_CNMF')
 [CC,jsf,im] = plot_contours(A_keep,Cn,options,1);
 
 gui2p_Calman3(CC,Cn,jsf,F_dff);
-%%
+%% for representative examples for spatial map
 % figure;
 % plot(F_dff(104,:))
 % plot_components_GUI(data,A_keep,C_keep,b,f,Cn,options);
