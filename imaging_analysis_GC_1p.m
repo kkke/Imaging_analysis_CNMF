01dcbc2d613edc6256659df580ecc8e8eb96f8fd
Ke Chen <kechen1.ck@gmail.com>
2019-10-07 19:46:30 -0400

    updated

--- imaging_analysis_GC_1p.m
+++ imaging_analysis_GC_1p.m
@@ -1,5 +1,7 @@
 %% data analysis code for 1p data
 % load imaging data
+path = 'G:\Imaging in GC\ImagingData\RVKC441\191001\1\RVKC441_191001_avg2_reg_source_extraction_reducemin06'
+cd(path)
 file = '03-Oct_21_21_21.mat';
 load(file)
 % remove fake neurons
@@ -7,45 +9,64 @@ load(file)
 
 % view neurons
 % i = 1;
-% neuron_new.save_neurons(); 
+neuron_new.save_neurons(); 
+
+% mannully clear the fake neurons
+
+neuron_new2 = fakeneuron_manual(neuron_new);
+
+
 % neuron_new.viewNeurons([]); 
-neuron.viewNeurons(1,neuron.C_raw);
+neuron.viewNeurons(1710,neuron.C_raw);
 Coor = neuron.show_contours(0.8);
 figure; 
-[CC,jsf] = plot_contours(neuron.A,neuron.Cn,);
-noise = [1334 1343 1329 1357 1345 1351 1336 1355 1350 1356, 594];
+[CC,jsf] = plot_contours(neuron.A,neuron.Cn,0.1,20,[],Coor);
 
-A = neuron.reshape(neuron.A, 2);
-imagesc(A(:,:,1))
-
+% A = neuron.reshape(neuron.A, 2);
+% imagesc(A(:,:,1))
+%% save processed roi
+save('neural_data.mat','neuron')
 %% load Intan data
 filename = 'RVKC441_191001.rhd';
-data = read_Intan(filename);
+[data,trial] = process_intan_v2(filename);
 [Frame,~] = Timing_onset_offset(data.event(7,:), data.ts, 0.5,10,0);
-[Tone,~]  = Timing_onset_offset(data.event(6,:), data.ts, 0.5,100,0);
-[W,~]     = Timing_onset_offset(data.event(5,:), data.ts, 0.5,10,0);
-[Q,~]     = Timing_onset_offset(data.event(4,:), data.ts, 0.5,10,0);
-[CA,~]    = Timing_onset_offset(data.event(3,:), data.ts, 0.5,10,0);
-[N,~]     = Timing_onset_offset(data.event(2,:), data.ts, 0.5,10,0);
-[S,~]     = Timing_onset_offset(data.event(1,:), data.ts, 0.5,10,0);
-[licks,~] = Timing_onset_offset(-data.analog(1,:), data.ts, -0.2,100,1);
+% there are trials without imaging; remove those trial
+k=1;
+for i = 1:length(trial)
+    if isempty(trial(i).Frame)
+        temp(k) = i;
+        k=k+1;
+    end
+end
+trial(temp)=[];
+clear temp
 
-% 
-
+% re-calculate the tiemstamps for frames; there is a 2-frame average to the
+% data
 for i = 1: floor(length(Frame)/2)
     ts_frame(i) = mean(Frame(2*i-1:2*i));
 end
 
+save('dataForCNMF.mat','trial','ts_frame','neuron','Coor')
 % visualize the calcium traces
-figure;
-plot(ts_frame,neuron.C_raw(1,:))
-hold on
-scatter(S,ones(size(S)),'ro','filled')
-scatter(N,ones(size(N)),'bo','filled')
-scatter(CA,ones(size(CA)),'yo','filled')
-scatter(Q,ones(size(Q)),'co','filled')
-scatter(W,ones(size(W)),'ko','filled')
+% figure;
+% plot(ts_frame,neuron.C_raw(1,:))
+% hold on
+% scatter(S,ones(size(S)),'ro','filled')
+% scatter(N,ones(size(N)),'bo','filled')
+% scatter(CA,ones(size(CA)),'yo','filled')
+% scatter(Q,ones(size(Q)),'co','filled')
+% scatter(W,ones(size(W)),'ko','filled')
 
-
+%% re-organize the data into trial structure
+for i = 1:length(trial)
+    temp = find(ts_frame> trial(i).tone-3 & ts_frame < trial(i).tone+12);
+    trial(i).Frame = ts_frame(temp)-trial(i).tone;
+    trial(i).S_trace = neuron.S(:,temp);
+    trial(i).C_raw_trace = neuron.C_raw(:,temp);  
+end
+[lickIni bout_dur] = lickIniateTime(trial);
+psth_cue(trial,6)
+psth_taste(trial,4)
 
 
