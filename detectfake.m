25ce301f5615d09b0e30ec1cca8c654d2fbd1cdc
Ke Chen <kechen1.ck@gmail.com>
2019-10-07 13:39:53 -0400

    fix bugs

--- detectfake.m
+++ detectfake.m
@@ -1,25 +1,26 @@
 function [fakeneuron, neuron_new] = detectfake(neuron)
+neuron_temp = neuron;
 
-for i = 1:size(neuron.C,1)
-    loc = find(neuron.C(i,:)>0.1);
+for i = 1:size(neuron_temp.C,1)
+    loc = find(neuron_temp.C(i,:)>0.1);
     duration = diff(loc);
     event(i) = realevent(duration); % detect the event; need to at least 5 frame cross the threshold
 end
 
 k =1;
 
-for i = 1:size(neuron.C,1)
+for i = 1:size(neuron_temp.C,1)
     if isempty(event(i).lickCount)
         fakeneuron(k) = i;
         k = k+1;
     end
 end
 
-neuron.A(:, fakeneuron)=[];
-neuron.C(fakeneuron,:) =[];
-neuron.C_raw(fakeneuron,:)=[];
-neuron.S(fakeneuron,:) =[];
-neuron.ids(fakeneuron) =[];
-neuron.tags(fakeneuron) =[];
-neuron_new = neuron;
+neuron_temp.A(:, fakeneuron)=[];
+neuron_temp.C(fakeneuron,:) =[];
+neuron_temp.C_raw(fakeneuron,:)=[];
+neuron_temp.S(fakeneuron,:) =[];
+neuron_temp.ids(fakeneuron) =[];
+neuron_temp.tags(fakeneuron) =[];
+neuron_new = neuron_temp;
 
