3b4426ccfa3a3642b7fb1ba181a36cc042f4599d
Ke Chen <kechen1.ck@gmail.com>
2019-10-08 20:29:43 -0400

    add summary

--- imaging_analysis_GC_1p.m
+++ imaging_analysis_GC_1p.m
@@ -59,6 +59,7 @@ save('dataForCNMF.mat','trial','ts_frame','neuron','Coor')
 % scatter(W,ones(size(W)),'ko','filled')
 
 %% re-organize the data into trial structure
+load('dataForCNMF.mat')
 taste = {'S','N','CA','Q','W'};
 for i = 1:length(trial)
     temp = find(ts_frame> trial(i).tone-3 & ts_frame < trial(i).tone+12);
@@ -71,11 +72,67 @@ for i = 1:length(trial)
         end
     end
 end
+% convert trial structure to neuron structure
 neuron_data = trial2neuron5tastant_1p(trial);
+% stats
 neuron_data = stats_1p(trial,neuron_data);
-length(find([neuron_data.CueRes]==0))/length(neuron_data);
+% length(find([neuron_data.CueRes]==0))/length(neuron_data);
+% check each individual neuron
+i=1182;
+psth_cue(trial,i)
+psth_taste(trial,i)
+psth_lick(trial,i)
+% imporse more strict criteria. check the overlap between cue and lick
+% response
+k=1;
+for i = 1:length(neuron_data)
+    if neuron_data(i).CueRes * neuron_data(i).LickRes ==1
+        overlap_cueLick(k) =i;
+        k = k+1;
+    end
+end
+resp_cueLick = stats_1p_overlapCueLick(trial,neuron, overlap_cueLick);
 
-psth_cue(trial,6)
-psth_taste(trial,4)
+% imporse more strict criteria. check the overlap between cue and lick
+% response
+k=1;
+for i = 1:length(neuron_data)
+    if neuron_data(i).S ==1 && neuron_data(i).N ==1 && neuron_data(i).CA ==1 && ...
+            neuron_data(i).Q ==1 && neuron_data(i).W == 1 && neuron_data(i).LickRes == 1
+        overlap_tasteLick(k) =i;
+        k = k+1;
+    elseif neuron_data(i).S ==-1 && neuron_data(i).N ==-1 && neuron_data(i).CA ==-1 && ...
+            neuron_data(i).Q ==-1 && neuron_data(i).W == -1 && neuron_data(i).LickRes == -1
+        overlap_tasteLick(k) =i;
+        k = k+1;        
+    end
+end
+resp_tasteLick = stats_1p_overlapTasteLick(trial,neuron, overlap_tasteLick);
+
+% include the strict criteria into the big data
+for i = 1:length(overlap_cueLick)
+    if resp_cueLick(i).LickRes ==0
+        neuron_data(overlap_cueLick(i)).LickRes = 0;
+    end   
+end
+
+for i = 1:length(overlap_tasteLick)
+    if resp_tasteLick(i).S ==0 && resp_tasteLick(i).N ==0 && resp_tasteLick(i).CA ==0 && ...
+            resp_tasteLick(i).Q ==0 && resp_tasteLick(i).W ==0      
+        neuron_data(overlap_tasteLick(i)).S =0;
+        neuron_data(overlap_tasteLick(i)).N =0;
+        neuron_data(overlap_tasteLick(i)).CA =0;
+        neuron_data(overlap_tasteLick(i)).W =0;
+        neuron_data(overlap_tasteLick(i)).Q =0;
+    end
+end
+
+%% now let's do some summary plots
+summary_1p(trial,neuron_data,neuron,Coor)
+summary_1p_v2(trial,neuron_data,neuron,Coor)
+
+
+
+
 
 
