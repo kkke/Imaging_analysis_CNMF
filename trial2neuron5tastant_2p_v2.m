1b77dd1e9055798cd0d1591a2c0a8bda9a1f37e9
Ke Chen <kechen1.ck@gmail.com>
2019-10-23 11:20:04 -0400

    trial to neuron for 2p

---
+++ trial2neuron5tastant_2p_v2.m
@@ -0,0 +1,8 @@
+function neuron_data = trial2neuron5tastant_2p_v2(neuron_data,trial)
+neuron_data_temp = trial2neuron5tastant_2p(trial);
+f = fieldnames(neuron_data_temp);
+for i = 1:length(f)
+    for j = 1:length(neuron_data)
+        neuron_data(j).(f{i}) = neuron_data_temp(j).(f{i});
+    end
+end
