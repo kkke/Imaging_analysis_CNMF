f611ff6356ba25ab86481f569f9d5db11390aaa8
Ke Chen <kechen1.ck@gmail.com>
2019-12-19 13:19:03 -0500

    for 2p

--- trial2neuron5tastant_2p_v2.m
+++ trial2neuron5tastant_2p_v2.m
@@ -1,5 +1,7 @@
 function neuron_data = trial2neuron5tastant_2p_v2(neuron_data,trial)
 neuron_data_temp = trial2neuron5tastant_2p(trial);
+% neuron_data_temp = trial2neuron5tastant_1p(trial);
+
 f = fieldnames(neuron_data_temp);
 for i = 1:length(f)
     for j = 1:length(neuron_data)
